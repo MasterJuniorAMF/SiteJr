cc0b45587bcb9b5eab772fc214efc9672f0ed82d
Master Junior AMF <masteramfjr@gmail.com>
2019-01-26 15:31:29 -0200

    Retoques função valida_email#12
    Cancelado o Refresh após validação do e-mail com a função (cancelado o refresh) event.preventDefault();

--- js/js_valida.js
+++ js/js_valida.js
@@ -7,11 +7,15 @@
 
             document.form1.email.focus();
 
+            event.preventDefault();/*Cancela o refresh da página*/
+
             return true;
 
         }else{
         
             alert("Favor inserir um email válido!");
+
+            event.preventDefault();/*Cancela o refresh da página*/
 
             document.form1.email.focus();
 
