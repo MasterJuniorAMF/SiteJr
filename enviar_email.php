f6a6be6a93851cc72e933328e8e18644433225ad
Master Junior AMF <masteramfjr@gmail.com>
2019-03-06 18:23:36 -0300

    #Ajustes envio

--- enviar_email.php
+++ enviar_email.php
@@ -1,15 +1,14 @@
 <?php
 
+	$email   = ($_POST['email']);
+	$subject = ($_POST['assunto']);
+	$txt = ($_POST['corpo']);
 	$email_remetente = "mailermasterjr@gmail.com";
 	$headers = "MIME-Version: 1.1\n";
 	$headers .= "Content-type: text/plain; charset=iso-8859-1\n";
 	$headers .= "From: $email_remetente\n"; // remetente
 	$headers .= "Return-Path: $email_remetente\n"; // return-path
 	$headers .= "Reply-To: $$email \n"; // Endereço (devidamente validado) que o seu usuário informou no contato
-	$txt = ($_POST['corpo']);
-	$subject = ($_POST['assunto']);
-	$to = "mailermasterjr@gmail.com";
-	$email   = ($_POST['email']);
 	
 	$retorno = mail("mailermasterjr@gmail.com", $subject, "$txt", $headers, "-f$email_remetente");
 
